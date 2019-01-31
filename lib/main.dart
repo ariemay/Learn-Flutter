6c824b6f14274a09bf74f226b11617dd782f7b6e
Arie May Wibowo <ariemayw@gmail.com>
2019-02-01 02:59:29 +0700

    End learning first session

--- lib/main.dart
+++ lib/main.dart
@@ -10,21 +10,23 @@ class HalamanSatu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new Container(
-        color: Colors.blue[200],
+      body:
+      new Center(
+      child: new Container(
+        color: Colors.blue[900],
         height: 100.0,
         width: 200.0,
         child: new Center(
-          child: new Text(
-            "Arie Ganteng",
-            style: new TextStyle(
-                color: Colors.red,
-                fontSize: 20.0,
-                fontFamily: "Serif",
-                fontWeight: FontWeight.bold),
+          // child: new Text(
+          //   "Shalecode",
+          //   style: new TextStyle(
+          //       color: Colors.white,
+          //       fontSize: 20.0,
+          //       fontFamily: "Serif",
+          //       fontWeight: FontWeight.bold),
+          child: new Icon(Icons.android, color: Colors.yellow, size: 70.0,),
           ),
         ),
-        alignment: Alignment(1.0, 1.0),
       ),
     );
   }
